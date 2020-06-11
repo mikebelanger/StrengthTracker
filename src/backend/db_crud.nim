3936630abd90b69addac9e8db020a587dcf68691
Mike Belanger <mbelanger@weathertelematics.com>
2020-06-11 09:59:38 -0400

    add get_foreign_keys function.

--- src/backend/db_crud.nim
+++ src/backend/db_crud.nim
@@ -1,47 +1,57 @@
-import allographer/query_builder
+import allographer/[query_builder, schema_builder]
 import json
 import schema_validation
 import create_database
-import strutils, strformat
+import strutils
+
+# HELPERS
+proc get_foreign_keys(db_columns: openArray[Column], input_json, return_json: JsonNode, id_search_term = "_id"): JsonNode =
+    
+    for column in db_columns:
+
+        if column.name.contains(id_search_term):
+            
+            let
+                real_name = column.name.replace(id_search_term, "")
+                thing = RDB().table(real_name)
+                             .select("id")
+                             .where("name", "=", input_json{real_name}.getStr).get()
+
+            if thing.len == 1:
+                var id = thing[0]{"id"}
+                return_json.add(key = column.name, val = %id)
+
+    return return_json
 
 # CREATE
 proc create_movement*(input_json: JsonNode) =
-    RDB().table("movement").insert(input_json)
+
+    if input_json.is_a_valid(movement):
+
+        try:
+
+            var to_insert = get_foreign_keys(db_columns = movement,
+                                            input_json = input_json,
+                                            return_json = %*{ "name" : input_json{"name"}})
+                
+
+            echo to_insert
+
+            RDB().table("movement").insert(to_insert)
+
+        except:
+            echo getCurrentExceptionMsg()
 
 
 if isMainModule:
     
     let some_json = parseJson("""{
-        "name": "Pull-up",
-        "movement_plane": "Horizontal",
+        "name": "Double KB Overhead press",
+        "movement_plane": "Vertical",
         "movement_category": "Push",
         "body_area": "Toes",
-        "movement_type": "Unilateral"
+        "movement_type": "Bilateral"
     }
     """)
 
-    echo some_json{"movement_plane"}.getStr
-
-    echo some_json.is_a_valid(movement)
-
-    var insert_json = %*{ "name" : some_json{"name"} }
-
-    for column in movement:
-
-        if column.name.contains("_id"):
-            
-            let 
-                real_name = column.name.replace("_id", "")
-                thing = RDB().table(real_name)
-                             .select("id")
-                             .where("name", "=", some_json{real_name}.getStr).get()
-
-            
-            if thing.len == 1:
-                var id = thing[0]{"id"}
-                insert_json.add(key = column.name, val = %id)
-
-    
-    echo insert_json
-
-    RDB().table("movement").insert(insert_json)
+    create_movement(some_json)
