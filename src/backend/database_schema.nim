5d575070d6f767387f4dd8f16ab0622743914888
Mike Belanger <mbelanger@weathertelematics.com>
2020-07-08 12:47:14 -0400

    made table variables private, as they're no longer accessed outside of the file

--- src/backend/database_schema.nim
+++ src/backend/database_schema.nim
@@ -3,8 +3,7 @@ import allographer/schema_builder
 import ../app_types
 
 let
-
-    movement_table* = [
+    movement_table = [
         Column().increments("id"),
         Column().string("name").unique(),
         Column().string("area"),
@@ -14,12 +13,12 @@ let
         Column().longText("description")
     ]
 
-    movement_combo_table* = [
+    movement_combo_table = [
         Column().increments("id"),
         Column().string("name").unique()
     ]
 
-    movement_combo_assignment_table* = [
+    movement_combo_assignment_table = [
         Column().increments("id"),
         Column().foreign("movement_id").reference("id").on("movement_combo").onDelete(SET_NULL),
         Column().foreign("movement_combo_id").reference("id").on("movement_combo").onDelete(SET_NULL)
