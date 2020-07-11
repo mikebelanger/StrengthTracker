6f067c947e0a0934c1b12ccc52f546cab9739f2a
Mike Belanger <mbelanger@weathertelematics.com>
2020-07-11 15:15:01 -0400

    add user, intenisty, routine, session, set tables

--- src/backend/database_schema.nim
+++ src/backend/database_schema.nim
@@ -39,13 +39,46 @@ let
         Column().foreign("movement_combo_id").reference("id").on("movement_combo").onDelete(SET_NULL)
     ]
 
-if isMainModule:
+    intensity_table = [
+        Column().increments("id"),
+        Column().float("quantity"),
+        Column().string("units")
+    ]
 
+    routine_table = [
+        Column().increments("id"),
+        Column().string("name").unique(),
+        Column().foreign("user_id").reference("id").on("user").onDelete(SET_NULL)
+    ]
+
+    session_table = [
+        Column().increments("id"),
+        Column().foreign("routine_id").reference("id").on("routine").onDelete(SET_NULL),
+        Column().datetime("session_date")
+    ]
+
+    set_table = [
+        Column().increments("id"),
+        Column().foreign("movement_id").reference("id").on("movement"),
+        Column().foreign("movement_combo_id").reference("id").on("movement_combo"),
+        Column().integer("reps"),
+        Column().string("tempo"),
+        Column().foreign("intensity_id").reference("id").on("intensity").onDelete(SET_NULL),
+        Column().foreign("session_id").reference("id").on("session").onDelete(SET_NULL),
+        Column().timestamp("duration"),
+        Column().integer("order")
+    ]
+
+if isMainModule:
 
     # # now add to db
     schema([
-        table($UserTable, movement_table),
+        table($UserTable, user_table),
         table($MovementTable, movement_table),
         table($MovementComboTable, movement_combo_table),
-        table($MovementComboAssignmentTable, movement_combo_assignment_table)
+        table($MovementComboAssignmentTable, movement_combo_assignment_table),
+        table($IntensityTable, intensity_table),
+        table($RoutineTable, routine_table),
+        table($SessionTable, session_table),
+        table($SetTable, set_table)
     ])
